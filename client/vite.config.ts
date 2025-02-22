bb12decc9fe62f48a7d192813b031e1832aa9ca5
kaztech069 <kaztech069@gmail.com>
2025-02-22 21:26:59 +0100

    kelp

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
     },
   },
   define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify('http://localhost:5000'), // Add the backend URL
+    'import.meta.env.VITE_API_URL': JSON.stringify(''), // Add the backend URL
   },
 });
